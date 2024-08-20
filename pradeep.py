e98d866c2d60346c8f6f4f318326e69ac0948f93
pradeepmega <pradeep.mail123@gmail.com>
2024-08-20 10:09:15 +0530

    Add files via upload

---
+++ pradeep.py
@@ -0,0 +1,10 @@
+import pyqrcode
+import streamlit as st
+s1="upi://pa=pradeep.mail123@okhdfcbank&am="
+s2=st.txt_input("Enter the amount")
+s3="&cu="INR"
+s=s1+s2+s3
+if st.button("Generate"):
+    url=pyqrcode.create(s)
+    url.png('myqr.png',scale=10)
+    st.image("myqr.png",caption="Generated QR")
