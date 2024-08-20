95b4178effa20529c54d67d39b44af8478fb2908
pradeepmega <pradeep.mail123@gmail.com>
2024-08-20 13:39:56 +0530

    pradeep.py

--- pradeep.py
+++ pradeep.py
@@ -2,7 +2,7 @@ import pyqrcode
 import streamlit as st
 s1="upi://pa=pradeep.mail123@okhdfcbank&am="
 s2=st.txt_input("Enter the amount")
-s3="&cu=INR"
+s3="&cu"="INR"
 s=s1+s2+s3
 if st.button("Generate"):
     url=pyqrcode.create(s)
