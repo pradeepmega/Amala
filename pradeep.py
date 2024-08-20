dc6b5b7c33b9b8118e4b7c613a70f1370fc4504f
pradeepmega <pradeep.mail123@gmail.com>
2024-08-20 15:17:24 +0530

    pradeep.py

--- pradeep.py
+++ pradeep.py
@@ -1,7 +1,7 @@
 import pyqrcode
 import streamlit as st
 s1="upi://pay?pa=pradeep.mail123@okhdfcbank&am="
-s2=st.txt_input("Enter the amount")
+s2=st.text_input("Enter the amount")
 s3="&cu=INR"
 s=s1+s2+s3
 if st.button("Generate"):
